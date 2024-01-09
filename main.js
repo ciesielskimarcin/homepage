18be35eeee50ee7ec41ca421fb991b059de30998
Marcin Ciesielski <marcin.ciesielski@edrmedeso.com>
2024-01-09 14:17:08 +0100

    changes from module V and below

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ console.log(`Nazywam sie ${firstName} i mam ${age} lata`);
 
 
 const p =document.querySelector('.article__paragraph--js')
-p.innerHTML='hej'
+p.innerHTML=' '
 
 
+function welcome (name, age) 
+{
+    return "My name is "+ name + " and I am " + age + " years old"; 
+}
+
+
+const result = welcome("darek", "23");
+console.log(welcome("Gnoj", 45));
